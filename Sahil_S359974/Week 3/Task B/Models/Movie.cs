37677d37102e80962b200eb9c8e92387edf66556
Sahil Hada <hadasahil786@gmail.com>
2023-07-28 09:50:03 +0930

    Started working on week 2 task B.

---
+++ Sahil_S359974/Week 3/Task B/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_B.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+
+        public string EmailAddress { get; set; }
+        public MovieLanguage Language { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public enum MovieLanguage { English = 0, Japanese = 1, Hindi = 2, Chinese = 3 }
+}
