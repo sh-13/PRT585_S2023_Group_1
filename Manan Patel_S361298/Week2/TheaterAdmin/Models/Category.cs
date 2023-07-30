18ad31a729424e5bbcf5dcf8bcd80fccf067f340
Manan Amitkumar Patel <mananpatel3008@gmail.com>
2023-07-30 15:30:52 +0930

    Theater Admin Task B - Not completed yet getting errors

---
+++ Manan Patel_S361298/Week2/TheaterAdmin/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheaterAdmin.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
