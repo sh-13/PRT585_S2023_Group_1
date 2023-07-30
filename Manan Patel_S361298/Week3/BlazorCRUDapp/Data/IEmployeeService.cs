ebd2ffa07331cbaa05122f3b129bda128d6e2e4e
Manan Amitkumar Patel <mananpatel3008@gmail.com>
2023-07-30 15:28:56 +0930

    Simple CRUD Operation in Blazer Server App

---
+++ Manan Patel_S361298/Week3/BlazorCRUDapp/Data/IEmployeeService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorCRUDapp.Data
+{
+    public interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        void UpdateEmployee(Employee employee);
+
+        void AddEmployee(Employee employee);
+
+        void DeleteEmployee(Guid id);
+    }
+}
---
+++ Manan Patel_S361298/Week3/Week3OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class Animal
+{
+    // abstract method
+
+    public abstract void animalSound();
+    
+    // Normal Method
+
+    public void sleep()
+    {
+        Console.WriteLine("ZZZZZZ");
+    }
+}
+
+class Cow : Animal
+{
+    public override void animalSound()
+    {
+        Console.WriteLine("The COW sounds");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cow cow = new Cow();
+        cow.animalSound();
+        cow.sleep();
+    }
+}
