96f1e3364758fa54aa575d8bed26a0b87c221bd9
Manan Amitkumar Patel <mananpatel3008@gmail.com>
2023-08-02 14:03:35 +0930

    MVC CRUD Operations Using Entity Framework Core and SQL Server

---
+++ Manan Patel_S361298/Week3/AspNetMCVCrud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AspNetMCVCrud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMCVCrud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public  DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Manan Patel_S361298/Week3/AspNetMCVCrud/Models/UpdateEmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AspNetMCVCrud.Models
+{
+    public class UpdateEmployeeViewModel
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public long Salary { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Department { get; set; }
+    }
+}
